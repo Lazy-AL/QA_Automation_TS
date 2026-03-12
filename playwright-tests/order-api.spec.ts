446eecbf5f4b2eab5a2747a45ca97cc1adebf6db
Anri-Laz <anri.lazashvilib@gmail.com>
2026-03-12 17:01:24 +0400

    Repeat until ready

--- playwright-tests/order-api.spec.ts
+++ playwright-tests/order-api.spec.ts
@@ -1,9 +1,33 @@
 import {test,expect} from "@playwright/test";
+import {create} from "node:domain";
 
-test('Create order return Processing', async({request}) =>{
+// test('Create order return Processing', async({request}) =>{
+//     const response = await request.post('http://localhost:3000/orders');
+//     const data = await response.json();
+//
+//     expect(response.status()).toBe(200);
+//     expect(data.status).toBe('PROCESSING');
+// })
+
+test('Repeat until pass', async ({request}) =>{
     const response = await request.post('http://localhost:3000/orders');
-    const data = await response.json();
+    const order = await response.json();
 
-    expect(response.status()).toBe(200);
-    expect(data.status).toBe('PROCESSING');
+    const orderId = order.id
+    const start = Date.now();
+
+    while(true){
+        const res = await request.get(`http://localhost:3000/orders/${orderId}`)
+        const data = await res.json()
+
+        if (data.status === 'READY'){
+            expect(data.status).toBe('READY')
+            break
+        }
+        if(Date.now() - start > 15000){
+            throw new Error('Order did not become Ready')
+        }
+
+        await new Promise(r => setTimeout(r, 500))
+    }
 })
