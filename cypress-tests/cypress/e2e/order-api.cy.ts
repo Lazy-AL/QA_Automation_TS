64244f0b90d3b1ef6e01ce7905c26e7b727de090
Anri-Laz <anri.lazashvilib@gmail.com>
2026-03-13 23:50:56 +0400

    Repeat until ready - cypress version

--- cypress-tests/cypress/e2e/order-api.cy.ts
+++ cypress-tests/cypress/e2e/order-api.cy.ts
@@ -1,3 +1,4 @@
+import {expect} from "@playwright/test";
 
 describe("Orders API", ()=>{
     it('creates orders', () =>{
@@ -5,4 +6,26 @@ describe("Orders API", ()=>{
             expect(response.status).to.eq(200)
         })
     })
+
+
+    it('should repeat assertion until return ready', () => {
+            cy.request('POST','/orders').then((createResponse) =>{
+                const orderId = createResponse.body.id
+
+                function checkOrder(){
+                    cy.request(`/orders/${orderId}`).then((res) =>{
+                        if (res.body.status === 'READY'){
+                            expect(res.body.status).to.eq('READY')
+                        }else {
+                            cy.wait(500)
+                            checkOrder()
+                        }
+                    })
+                }
+                    checkOrder()
+
+            })
+    });
+
+
 })
