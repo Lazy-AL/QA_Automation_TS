3f8ab3e51d880e750dd3a9290da1cfced336334c
Anri-Laz <anri.lazashvilib@gmail.com>
2026-04-06 23:45:37 +0400

    services

--- shared/services/orderService.ts
+++ shared/services/orderService.ts
@@ -1,15 +1,15 @@
-// export const fetchOrder = async (getOrderFn: any, id: number) => {
-//     const res = await getOrderFn(id)
-//
-//     if (res.status !== 200) {
-//         throw new Error('Failed to Fetch')
-//     }
-//
-//     return {
-//         id: res.body.id,
-//         status: res.body.status
-//     }
-// }
+export const fetchOrder = async (getOrderFn: any, id: number) => {
+    const res = await getOrderFn(id)
+
+    if (res.status !== 200) {
+        throw new Error('Failed to Fetch')
+    }
+
+    return {
+        id: res.body.id,
+        status: res.body.status
+    }
+}
 
 export const createOrderAndWaitReady = async (createOrderFn:any,waitFn:any)=>{
     const order = await createOrderFn()
