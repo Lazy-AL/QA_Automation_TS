802c0d975e3d171096e147d1a13156ced79dd291
Anri-Laz <anri.lazashvilib@gmail.com>
2026-04-07 23:47:56 +0400

    services

--- steps/order.steps.ts
+++ steps/order.steps.ts
@@ -4,14 +4,14 @@ import { createOrder } from '../shared/api/ordersClient.pw'
 import { awaitForOrderReady } from '../shared/helpers/waitForOrderReady.pw'
 import { createOrderAndWaitReady } from '../shared/services/orderService'
 
-Given('I create an order', async function () {
-    this.apiContext = await pwRequest.newContext()
-
-    this.result = await createOrderAndWaitReady(
-        () => createOrder(this.apiContext),
-        (id) => awaitForOrderReady(this.apiContext, id)
-    )
-})
+// Given('I create an order', async function () {
+//     this.apiContext = await pwRequest.newContext()
+//
+//     this.result = await createOrderAndWaitReady(
+//         () => createOrder(this.apiContext),
+//         (id) => awaitForOrderReady(this.apiContext, id)
+//     )
+// })
 
 Then('the order status should be {string}', function (status) {
     expect(this.result.status).toBe(status)
