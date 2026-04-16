02c7e526aef803cabae068301051cf592cff1c06
Anri-Laz <anri.lazashvilib@gmail.com>
2026-04-16 20:15:02 +0400

    services

--- steps/order.steps.ts
+++ steps/order.steps.ts
@@ -1,18 +1,18 @@
-// import { Given, Then } from '@cucumber/cucumber'
-// import { expect, request as pwRequest } from '@playwright/test'
-// import { createOrder } from '../shared/api/ordersClient.pw'
-// import { awaitForOrderReady } from '../shared/helpers/waitForOrderReady.pw'
-// import { createOrderAndWaitReady } from '../shared/services/orderService'
-//
-// Given('I create an order', async function () {
-//     this.apiContext = await pwRequest.newContext()
-//
-//     this.result = await createOrderAndWaitReady(
-//         () => createOrder(this.apiContext),
-//         (id) => awaitForOrderReady(this.apiContext, id)
-//     )
-// })
-//
-// Then('the order status should be {string}', function (status) {
-//     expect(this.result.status).toBe(status)
-// })
+import { Given, Then } from '@cucumber/cucumber'
+import { expect, request as pwRequest } from '@playwright/test'
+import { createOrder } from '../shared/api/ordersClient.pw'
+import { awaitForOrderReady } from '../shared/helpers/waitForOrderReady.pw'
+import { createOrderAndWaitReady } from '../shared/services/orderService'
+
+Given('I create an order', async function () {
+    this.apiContext = await pwRequest.newContext()
+
+    this.result = await createOrderAndWaitReady(
+        () => createOrder(this.apiContext),
+        (id) => awaitForOrderReady(this.apiContext, id)
+    )
+})
+
+Then('the order status should be {string}', function (status) {
+    expect(this.result.status).toBe(status)
+})
